bb857f1af57fd87428687573540a054667aa2d6f
lennar6 <lennytarcy@gmail.com>
2026-08-20 11:52:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sparna-v2';
+const CACHE = 'sparna-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
